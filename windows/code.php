d017f651494ed195c0d8b8ac9ad75b869869fb06
BiZoNiX <bizonix@me.com>
2015-12-12 02:53:54 +0200

    Code analysis: - added vulnerability type 'XPath Injection' - implemented $F_INSECURING_STRING (list of functions like urldecode() that can re-taint already sanitized data) - fixed bug with $GLOBALS[] (ignore previous local vars, accept only global vars) - improved tainted $_SESSION (=global var) handling - fixed bug with tainting functions not displayed in the result tree - fixed bug with differently used quotes in array['"parameter'"] during traceback - added compact() support - ignore upper/lowercase in function names because PHP does not (sYsTem()) - scan for dynamic function calls $variable() (possible code exec)
    Interface:
    - added missing taint-highlighting in the first line of PVF tree
    - added file list: listing all scanned files and includes
    - added list of function calls to each list item of user-defined functions
    - added help button for simple visualization, description, example, PoC, patch and securing function list in a new window
    - RIPS warns you when scan may last very long (counts files to scan)
    - added AJAX interface with scan animation
    - added scan result statistics and graphs
    - highlight variables onMouseOver in code viewer and scan result. persistent highlight onClick.
    - code viewer now supports active jumping between function calls and declarations
    	(click on function call to jump to declaration, click "return" to jump back to the call)
    - added regex search function
    - windows are now resizeable
    - added curl headers for all tainting $_SERVER parameters in the exploit creator

---
+++ windows/code.php
@@ -0,0 +1,93 @@
+<table width='100%'>
+<?php
+/** 
+
+RIPS - A static source code analyser for vulnerabilities in PHP scripts 
+	by Johannes Dahse (johannesdahse@gmx.de)
+			
+			
+Copyright (C) 2010 Johannes Dahse
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.	
+
+**/
+
+	// prepare output to style with CSS
+	function highlightline($line, $line_nr, $marklines)
+	{
+		$tokens = @token_get_all('<? '.$line.' ?>');
+		
+		$output = (in_array($line_nr, $marklines)) ? '<tr><td nowrap class="markline">' : '<tr><td nowrap>';
+
+		foreach ($tokens as $token)
+		{
+			if (is_string($token))
+			{		
+				$output .= '<span class="phps-code">';
+				$output .= htmlentities($token, ENT_QUOTES, 'utf-8');
+				$output .= '</span>';
+			} 
+			else if (is_array($token) 
+			&& $token[0] !== T_OPEN_TAG
+			&& $token[0] !== T_CLOSE_TAG) 
+			{					
+				if ($token[0] !== T_WHITESPACE)
+				{
+					$text = '<span ';
+					if($token[0] === T_VARIABLE)
+					{
+						$cssname = str_replace('$', '', $token[1]);
+						$text.= 'style="cursor:pointer;" name="phps-var-'.$cssname.'" onClick="markVariable(\''.$cssname.'\')" ';
+						$text.= 'onmouseover="markVariable(\''.$cssname.'\')" onmouseout="markVariable(\''.$cssname.'\')" ';
+					}	
+					else if($token[0] === T_STRING)
+					{
+						$text.= "onmouseover=\"mouseFunction('{$token[1]}', this)\" onmouseout=\"this.style.textDecoration='none'\" ";
+						$text.= "onclick=\"openFunction('{$token[1]}','$line_nr');\" ";
+					}
+					$text.= 'class="phps-'.str_replace('_', '-', strtolower(token_name($token[0]))).'" ';
+					$text.= '>'.htmlentities($token[1], ENT_QUOTES, 'utf-8').'</span>';
+				}
+				else
+				{
+					$text = str_replace(' ', '&nbsp;', $token[1]);
+					$text = str_replace("\t", str_repeat('&nbsp;', 8), $text);
+				}
+				
+				$output .= $text;
+			}
+		}
+		return $output.'</td></tr>';
+	}
+	
+	// print source code and mark lines
+	
+	$file = $_GET['file'];
+	$marklines = explode(',', $_GET['lines']);
+
+	
+	if(!empty($file))
+	{
+		$lines = file($file);
+		
+		// place line numbers in extra table for more elegant copy/paste without line numbers
+		echo '<tr><td><table>';
+		for($i=1, $max=count($lines); $i<=$max;$i++) 
+			echo "<tr><td class=\"linenrcolumn\"><span class=\"linenr\">$i</span><A id='".($i+2).'\'></A></td></tr>';
+		echo '</table></td><td><table width="100%">';
+		
+		for($i=0; $i<$max; $i++)
+		{
+			echo highlightline($lines[$i], $i+1, $marklines);
+		}
+	} else
+	{
+		echo '<tr><td>No file specified.</td></tr>';
+	}
+?>
+</table>
+</td></tr></table>
